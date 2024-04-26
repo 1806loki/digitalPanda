090d1dfa3d795db864809e8d149ef9056185b5d7
1806loki <127595726+1806loki@users.noreply.github.com>
2024-04-26 14:30:29 +0530

    Finishing Auth Flow

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,10 @@ import { webpackBundler } from "@payloadcms/bundler-webpack"
 import path from "path"
 import { Users } from "./collections/users"
 
+import dotenv from 'dotenv';
+dotenv.config({
+    path: path.resolve(__dirname, "../.env")
+})
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
     collections: [Users],
